5c37097583228193dbddd7738b42d45c2dd86710
paaras arora <88595024+paarasarora@users.noreply.github.com>
2024-01-03 17:43:51 +0530

    fullstack chat app

--- chat/pusher.py
+++ chat/pusher.py
@@ -1,9 +1,8 @@
 import pusher
 
 pusher_client = pusher.Pusher(
-    app_id='',
-    key='',
-    secret='',
-    cluster='',
-    ssl=True
+    app_id='1728358',
+    key='5f3afa97b37fea63cef9',
+    secret='62cbc3c7b4b92414002e',
+    cluster='ap2'
 )
